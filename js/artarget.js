961cfd77a117e3993e2121b04a4da08b0d9ec090
iliasom <i9091780@gmail.com>
2026-08-18 21:06:59 +0545

    css2rend

--- js/artarget.js
+++ js/artarget.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { CSS2DRenderer, CSS2DObject } from './CSS3DRenderer';
+import { CSS2DRenderer, CSS2DObject } from './CSS3DRenderer.js';
 
 let cssRendererInstance = null;
 
